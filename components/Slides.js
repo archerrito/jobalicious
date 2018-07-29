acf84d546bcba06f95ba9e86a17cb2eb5b2d8c1a
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-29 17:56:57 -0500

    add slide component to welcome screen with basic styling

---
+++ components/Slides.js
@@ -0,0 +1,37 @@
+import React, {Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+
+class Slides extends Component {
+    renderSlides() {
+        return this.props.data.map((slide) => {
+            return (
+                <View key={slide.text} style = {styles.slideStyle}>
+                    <Text style={styles.textStyle}>{slide.text}</Text>
+                </View>
+            );
+        });
+    }
+    render() {
+        return (
+            <ScrollView
+                horizontal={true}
+                style={{ flex: 1 }}
+            >
+            {this.renderSlides()}
+            </ScrollView>
+        );
+    }
+}
+
+const styles= {
+    slideStyle: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textStyle: {
+        fontSize: 30
+    }
+};
+
+export default Slides;
