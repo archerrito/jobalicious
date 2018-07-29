b489e5af364b874f25f0581c3979141028dff1a5
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-29 17:10:55 -0500

    add review header flow

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 class ReviewScreen extends Component {
+    //class properties
+    static navigationOptions = ({ navigation }) => ({
+        title: 'Review Jobs',
+        headerRight: <Button title="Settings" onPress={() => { navigation.navigate('settings') }} />
+    })
     render() {
         return (
             <View>
