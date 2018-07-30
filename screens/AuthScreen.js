120220c83c88147deb29c8407675a93416de7834
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-29 21:53:31 -0500

    clean up of auth reducer flow

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -6,21 +6,32 @@ import * as actions from '../actions';
 class AuthScreen extends Component {
     componentDidMount() {
         this.props.facebookLogin();
-        AsyncStorage.removeItem('fb_token');
+        this.onAuthComplete(this.props);
+    }
+
+    //called when component about to re-render
+    componentWillReceiveProps(nextProps) {
+        this.onAuthComplete(nextProps);
+    }
+
+    onAuthComplete(props) {
+        if (props.token) {
+            this.props.navigation.navigate('map');
+        }
     }
     render() {
         return (
-            <View>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-            </View>
+            <View />
         )
     }
 }
 
+//wire up auth screen to auth piece of state in app
+//so auth screen knows current state of authentication is
+//look at token property
+function mapStateToProps({ auth }){
+    return { token: auth.token };
+}
+
 //take actions, bind ot authscreen
-export default connect(null, actions)(AuthScreen);
+export default connect(mapStateToProps, actions)(AuthScreen);
