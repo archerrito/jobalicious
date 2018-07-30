a461832780eb8739e889cb1dd8b651bb88491c30
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-30 06:26:35 -0500

    add action filess for job api

---
+++ actions/job_actions.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+import {
+    FETCH_JOBS
+} from './types';
+
+//will make network request with thunk
+//pass in region object to get list of jobs
+export const fetchJobs = (region) => async (dispatch) =>{
+    
+}
