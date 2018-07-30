277508da03f51c1022fb46034d6915e14a3dd5a6
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-30 06:44:11 -0500

    add API request to indeed

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -6,6 +6,7 @@ import {
     FETCH_JOBS
 } from './types';
 
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
 const JOB_QUERY_PARAMS = {
     publisher: '4201738803816157',
     format: 'json',
@@ -15,12 +16,24 @@ const JOB_QUERY_PARAMS = {
     q: 'javascript'
 };
 
+const buildJobsUrl = (zip) => {
+    //pass in object, pass in zip object
+    const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip});
+    //took root, then appended query params
+    return `${JOB_ROOT_URL}${query}`;
+}
+
 //will make network request with thunk
 //pass in region object to get list of jobs
 export const fetchJobs = (region) => async (dispatch) =>{
     try {
         //contains lat/lng
         let zip = await reverseGeocode(region);
+        const url = buildJobsUrl(zip);
+        //qill make request, return response object
+        let { data } = await axios.get(url);
+        dispatch({ type: FETCH_JOBS, payload: data });
+        console.log(data);
     } catch(e) {
         console.error(e);
     }
